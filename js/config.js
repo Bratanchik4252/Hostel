7b9925570e8fab81e840bac60cbea6dc383bbe23
Bratanchik4252 <4252@users.noreply.github.com>
2026-08-02 14:41:13 +0300

    Web3Forms: новый access key (аккаунт hostel_zakaz@mail.ru)

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 window.SITE_CONFIG = {
   // Ключ Web3Forms (https://web3forms.com): письмо на почту + копия в их Google Таблицу.
   // Зарегистрируйтесь с почты hostel_zakaz@mail.ru, создайте форму и вставьте сюда свой access key.
-  web3Key: "41087e69-7ee8-4514-a814-47930b3e8d90",
+  web3Key: "584d3229-c6d5-4e38-b65a-c6fc425805d1",
 
   // Контакты, которые показываются на сайте.
   contactPhone: "+7 4922 37-03-37",
